b59aa6b8a773b3a845d94451058e89a1c2ab5f92
babas2 <79476483+babas2@users.noreply.github.com>
2021-03-18 19:40:26 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = ({
-	token: "1748522817:AAEnkWpe1YpvOd8Mo-OO_3tkEHid7XyMQ8I", //your bot token
-	botId: "1748522817" , //your bot token's first digits
+	token: "1758648009:AAEAiZKtIBvpCwwtYAs7D2oi_93mdNrG458", //your bot token
+	botId: "1758648009" , //your bot token's first digits
 	rounds: 5,
 	waitDelay: 12000,
 	timerSteps: 3,
